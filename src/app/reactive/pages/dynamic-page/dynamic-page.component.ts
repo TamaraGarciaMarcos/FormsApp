c6bc2da7503e86cbfac79713b5ba1b5e391cd149
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-15 08:55:13 +0200

    validaciones del array de  favoritos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,63 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: ``
+  styles: []
 })
 export class DynamicPageComponent {
 
+  public myForm : FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3)]],
+    favoritesGames: this.fb.array([
+      ['Stardew Valley', Validators.required],
+      ['The legend of Zelda', Validators.required],
+      ['Pokémon', Validators.required]
+    ])
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  get favoritesGames(){
+    return this.myForm.get('favoritesGames') as FormArray;
+  }
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray( formArray: FormArray, index: number){
+    return formArray.controls[index].errors
+        && formArray.controls[index].touched;
+  }
+
+  getFieldError(field: string): string | null{
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors= this.myForm.controls[field].errors || {};
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido'
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`
+      }
+    }
+    return null;
+  }
+
+  onDeleteFAvorite( index: number):void{
+    this.favoritesGames.removeAt(index)
+  }
+
+  onSubmit():void{
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
